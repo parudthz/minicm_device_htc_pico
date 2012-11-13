69910cd7a2bb27e8caf181e3b0a44ebb3c3a9c2a
Fariz Rachman <parudthz@gmail.com>
2012-11-13 09:11:40 +0700

    update pico.mk

--- pico.mk
+++ pico.mk
@@ -52,11 +52,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=CyanPing.ogg \
     ro.config.alarm_alert=CyanAlarm.ogg
 
-# A2SD and extra init files
+# D2EXT and extra init files
 PRODUCT_COPY_FILES += \
-    device/htc/pico/prebuilt/bin/a2sd:system/bin/a2sd \
     device/htc/pico/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
     device/htc/pico/prebuilt/etc/init.d/04modules:system/etc/init.d/04modules \
+    device/htc/pico/prebuilt/etc/init.d/05swap:system/etc/init.d/05swap \
+    device/htc/pico/prebuilt/etc/init.d/06minicm:system/etc/init.d/06minicm \
     device/htc/pico/prebuilt/etc/init.d/40d2ext:system/etc/init.d/40d2ext \
     device/htc/pico/prebuilt/xbin/zipalign:system/xbin/zipalign
 
